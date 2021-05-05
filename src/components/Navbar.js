67edaa19a2031ace73921ee26fc5aa34aba6af26
Damian Janus <damian.janus@gmail.com>
2021-05-05 18:00:22 +0200

    Add another context and check how to use multiple contex

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,36 @@
 import React from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 // In here we use Context Consumer technique
 const Navbar = () => {
   return (
-    <ThemeContext.Consumer>
-      {context => {
-        const { isLightTheme, lightTheme, darkTheme } = context;
-        console.log(context)
-        const theme = isLightTheme ? lightTheme : darkTheme;
-        return (
-          <nav style={{backgroundColor: theme.colors.backgroundColor, color: theme.colors.textColor}}>
-            <h1>Context App</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
+    <AuthContext.Consumer>{
+      authContext => (
+          <ThemeContext.Consumer>
+            {themeContext => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, lightTheme, darkTheme } = themeContext;
+              const theme = isLightTheme ? lightTheme : darkTheme;
+              
+              return (
+                <nav style={{backgroundColor: theme.colors.backgroundColor, color: theme.colors.textColor}}>
+                  <h1>Context App</h1>
+                  <div onClick={toggleAuth}>
+                    { isAuthenticated ? 'Logged In 😻' : 'Logged Out 😿'}
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              )
+            }}
+          </ThemeContext.Consumer> 
         )
-      }}
-    </ThemeContext.Consumer> 
+      }
+    </AuthContext.Consumer>
   );
 }
  
